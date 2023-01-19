faac6683133b5c8de0b207ce9e995add42b8326c
kai2468 <kai246@hotmail.co.uk>
2023-01-19 20:05:06 +0100

    Add files via upload

---
+++ everviz-get-charts/index.js
@@ -0,0 +1,24 @@
+const fetch = require('node-fetch');
+
+const cmsKey = '',
+      TEAM_ID = '',
+      pageSize = 15,
+      page = 1,
+      search = '';
+
+let	url = `https://api.everviz.com/team/${TEAM_ID}/projects/search/published?page=${page}&pageSize=${pageSize}&query=${search}`;
+
+fetch(url, {
+  method: 'GET',
+  headers: { 
+  	'Content-Type': 'application/json',
+  	'X-CMS-Key': cmsKey
+   },
+})
+.then(res => res.json())
+.then(json => console.log(json))
+.catch((e) => {
+	console.log(e);
+});
+
+
